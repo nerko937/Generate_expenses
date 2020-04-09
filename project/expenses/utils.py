fa63fb81b2ca044af4408051ecf7377c008112cc
Marcin Fudala <marcinfudala744@gmail.com>
2020-04-09 19:18:12 +0200

    final excel improvements

--- project/expenses/utils.py
+++ project/expenses/utils.py
@@ -36,12 +36,14 @@ def generate_xlsx(month):
     file_path = os.path.join(DOWNLOADS_PATH, f'{month.user_id}.xlsx')
     workbook = xlsxwriter.Workbook(file_path)
     worksheet = workbook.add_worksheet()
-    worksheet.set_column(1, 2, 13)
+    worksheet.set_column(1, 1, 13)
+    worksheet.set_column(2, 2, 150)
 
     # format variables
     bold = workbook.add_format({'bold': True})
     title = workbook.add_format({'bold': 1, 'align': 'center'})
     vcenter = workbook.add_format({'valign': 'vcenter'})
+    wrap = workbook.add_format(({'text_wrap': True}))
 
     # title and headers
     worksheet.merge_range('A1:C1', f'{MONTHS[month.month][1]} {month.year}', title)
@@ -52,38 +54,39 @@ def generate_xlsx(month):
     # body
     row = 2                 # row after headers
     cr_category = ''
-    categories_ranges = []  # list of lists with category name and coordinates for sum count
+    sum_total = 0
+    categories_ranges = []  # list of lists with sum, category name and coordinates for excel sum coords
     for expense in month.expenses:
-        height = 14
-        if len(expense.description) > 150:
-            height = math.ceil(len(expense.description) / 150) * 8
-            worksheet.set_row(row, height)   # set more row height
-
-        # write row
         worksheet.write(row, 0, expense.amount, vcenter)
         worksheet.write(row, 1, expense.category, vcenter)
-        worksheet.insert_textbox(
-            row, 2, expense.description, {'width': 1000, 'height': height * 1.3}
-        )
+        worksheet.write(row, 2, expense.description, wrap)
 
         # categories_ranges appends
         if expense.category != cr_category:
             if len(categories_ranges):
                 categories_ranges[-1].append(row - 1)
             cr_category = expense.category
-            categories_ranges.append([cr_category, row])
+            categories_ranges.append([expense.amount, cr_category, row])
+        else:
+            categories_ranges[-1][0] += expense.amount
+        sum_total += expense.amount
         
         # increment
         row += 1
+    
+    worksheet.merge_range(f'A{row + 1}:B{row + 1}', 'Summary', title)
+    row += 1
 
     # summary
     if len(categories_ranges):
         categories_ranges[-1].append(row - 1) # finish last coordinate
-        categories_ranges.append(['Total', 2, row - 1])
-        # headers
-        worksheet.write_row(row, 0, (el[0] for el in categories_ranges), bold)
-        # values
-        worksheet.write_row(row + 1, 0, (f'=SUM(A{el[1] + 1}:A{el[2] + 1}' for el in categories_ranges))
+        categories_ranges.append([sum_total, 'Total', 2, row - 1])
+        for sum_cat_data in categories_ranges:
+            worksheet.write_formula(                    # sum
+                row, 0, f'=SUM(A{sum_cat_data[2] + 1}:A{sum_cat_data[3] + 1}', value=sum_cat_data[0]
+            )
+            worksheet.write(row, 1, sum_cat_data[1])    # category
+            row += 1
 
     # close
     workbook.close()
